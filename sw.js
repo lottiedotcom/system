3e031fbc778a2be569652994f1d1af7780ce2fe0
lottiedotcom <notilottie@gmail.com>
2026-01-07 18:38:42 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'seiren-os-v19';
+const CACHE_NAME = 'seiren-os-v22'; // Updated version number
 const urlsToCache = [
   './',
   './index.html',
@@ -6,9 +6,24 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting(); // Forces new service worker to take over immediately
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Deletes old versions
+          }
+        })
+      );
+    })
   );
 });
 
