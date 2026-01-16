b29e54a8362b42fc645d795b25e33e58aff024d3
lottiedotcom <notilottie@gmail.com>
2026-01-16 11:49:10 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,37 @@
-const CACHE_NAME = 'seiren-os-v59'; 
+const CACHE_NAME = 'seiren-os-v88-force-update';
 const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  'https://cdn-icons-png.flaticon.com/512/2919/2919573.png'
+    './',
+    './index.html',
+    './manifest.json',
+    'https://cdn-icons-png.flaticon.com/512/2919/2919573.png'
 ];
 
+// INSTALL: Force new version to jump the line
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+    self.skipWaiting(); // THIS LINE IS CRITICAL FOR UPDATES
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
+// ACTIVATE: Delete all old caches immediately
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keyList) => Promise.all(keyList.map((key) => {
-    if (key !== CACHE_NAME) return caches.delete(key);
-  }))));
-  self.clients.claim();
+    e.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('Removing old cache:', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    self.clients.claim(); // Grab control of the page immediately
 });
 
+// FETCH: Try Cache, then Network
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).then((res) => {
-    const clone = res.clone();
-    caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
-    return res;
-  }).catch(() => caches.match(e.request)));
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            return res || fetch(e.request);
+        })
+    );
 });
