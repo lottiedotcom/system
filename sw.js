359c647df45a07fbb9a2e56fa0b044658a8bda4e
lottiedotcom <notilottie@gmail.com>
2026-01-16 08:42:27 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,17 @@
-const CACHE_NAME = 'seiren-os-v51'; 
+const CACHE_NAME = 'seiren-os-v52'; 
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  // System Icons
   'https://cdn-icons-png.flaticon.com/512/2919/2919573.png',
+  // Default Profile Avatar
   'https://cdn-icons-png.flaticon.com/512/3233/3233508.png',
-  'https://cdn-icons-png.flaticon.com/512/3069/3069172.png'
+  // New Ebi (Pet) Image
+  'https://i.postimg.cc/13FQckct/Untitled-Artwork-20260110072840-20260110074706.png'
 ];
 
+// Install Event
 self.addEventListener('install', (e) => {
   self.skipWaiting();
   e.waitUntil(
@@ -17,6 +21,7 @@ self.addEventListener('install', (e) => {
   );
 });
 
+// Activate Event (Cleanup Old Caches)
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keyList) => {
@@ -32,10 +37,12 @@ self.addEventListener('activate', (e) => {
   self.clients.claim();
 });
 
+// Fetch Event (Offline Capability)
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     fetch(e.request)
       .then((res) => {
+        // Cache new files automatically
         const clone = res.clone();
         caches.open(CACHE_NAME).then((cache) => {
           cache.put(e.request, clone);
