c8c45c9ce2943d2d16b6eac5e7cf73f5759babb6
lottiedotcom <notilottie@gmail.com>
2026-01-16 15:23:09 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'seiren-os-v72-force-update';
+const CACHE_NAME = 'seiren-os-v76-pk-update';
 const ASSETS = [
     './',
     './index.html',
@@ -6,33 +6,39 @@ const ASSETS = [
     'https://cdn-icons-png.flaticon.com/512/2919/2919573.png'
 ];
 
-// 1. INSTALL: Force the new worker to jump the line
+// 1. INSTALL: Force the new worker to skip the waiting line
 self.addEventListener('install', (e) => {
     self.skipWaiting(); 
     e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// 2. ACTIVATE: Delete all old versions of the site immediately
+// 2. ACTIVATE: Delete ALL old caches immediately
 self.addEventListener('activate', (e) => {
     e.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
                 if (key !== CACHE_NAME) {
-                    console.log('Cleaning up old cache:', key);
+                    console.log('Nuking old cache:', key);
                     return caches.delete(key);
                 }
             }));
         })
     );
-    self.clients.claim(); // Take control of the page right away
+    self.clients.claim(); // Grab control of the page immediately
 });
 
-// 3. FETCH: Serve cached files if available, otherwise fetch from network
+// 3. FETCH: Network First, then Cache
+// This ensures you always get the live version if you have internet.
 self.addEventListener('fetch', (e) => {
     e.respondWith(
-        caches.match(e.request).then((res) => {
-            return res || fetch(e.request);
-        })
+        fetch(e.request)
+            .then((res) => {
+                const clone = res.clone();
+                caches.open(CACHE_NAME).then((cache) => cache.put(e.request, clone));
+                return res;
+            })
+            .catch(() => {
+                return caches.match(e.request);
+            })
     );
 });
-
