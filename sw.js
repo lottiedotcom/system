6a18418576b1e6bbaa39bac26bd1225465e0d586
lottiedotcom <notilottie@gmail.com>
2026-01-16 16:51:51 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'seiren-os-v76-pk-update';
+const CACHE_NAME = 'seiren-os-v78-final';
 const ASSETS = [
     './',
     './index.html',
@@ -6,29 +6,24 @@ const ASSETS = [
     'https://cdn-icons-png.flaticon.com/512/2919/2919573.png'
 ];
 
-// 1. INSTALL: Force the new worker to skip the waiting line
 self.addEventListener('install', (e) => {
     self.skipWaiting(); 
     e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// 2. ACTIVATE: Delete ALL old caches immediately
 self.addEventListener('activate', (e) => {
     e.waitUntil(
         caches.keys().then((keyList) => {
             return Promise.all(keyList.map((key) => {
                 if (key !== CACHE_NAME) {
-                    console.log('Nuking old cache:', key);
                     return caches.delete(key);
                 }
             }));
         })
     );
-    self.clients.claim(); // Grab control of the page immediately
+    self.clients.claim(); 
 });
 
-// 3. FETCH: Network First, then Cache
-// This ensures you always get the live version if you have internet.
 self.addEventListener('fetch', (e) => {
     e.respondWith(
         fetch(e.request)
