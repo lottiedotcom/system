5b43a0698ccd5155b316a020b42a836118d6fffa
lottiedotcom <notilottie@gmail.com>
2026-01-16 07:06:42 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,61 +1,46 @@
-const CACHE_NAME = 'seiren-cache-v1';
-const URLS_TO_CACHE = [
-    './',
-    './index.html',
-    './manifest.json'
+const CACHE_NAME = 'seiren-os-v50'; 
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/2919/2919573.png',
+  'https://cdn-icons-png.flaticon.com/512/3233/3233508.png' 
 ];
 
-// Install: Cache core files
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(URLS_TO_CACHE);
-            })
-    );
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-// Activate: Clean up old caches
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-    );
+      );
+    })
+  );
+  self.clients.claim();
 });
 
-// Fetch: Serve from cache, or fetch and cache new items (like avatars)
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Return cached response if found
-                if (response) {
-                    return response;
-                }
-                
-                // Otherwise fetch from network
-                return fetch(event.request).then((networkResponse) => {
-                    // Check if valid response
-                    if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic' && networkResponse.type !== 'cors') {
-                        return networkResponse;
-                    }
-
-                    // Clone response to cache it
-                    const responseToCache = networkResponse.clone();
-                    caches.open(CACHE_NAME).then((cache) => {
-                        cache.put(event.request, responseToCache);
-                    });
-
-                    return networkResponse;
-                });
-            })
-    );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request)
+      .then((res) => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(e.request, clone);
+        });
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
 });
